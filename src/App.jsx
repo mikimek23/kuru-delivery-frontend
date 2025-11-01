b099f8afee0a78d35c5b14ba4986f3d3445c0d1d
unknown <mikimek933@gmail.com>
2025-11-01 18:33:45 +0300

    folder structure is created

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+//use
+// primary:#FF6600;
+// secondary:#1A1A1A;
+// background:#F1F1F1;
+// like one in the example
+function App() {
+
+  return (
+   <div className="bg-background text-secondary border-primary border-2">APP</div>
+  )
+}
+export default App
